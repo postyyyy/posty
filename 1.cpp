8d09f3e56c41c4e43213a263e8adfb00d2ff0a1e
posty afk utkarsh <83517549+postyyyy@users.noreply.github.com>
2021-07-07 00:10:54 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,27 +1,33 @@
-//question- find whether the number has unique digits or not???
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
-    bool arr[10] = {false};
-    int r;
+    int n1,n2;
+    cin >> n1>>n2;
 
-    while (n > 0)
-    {
-        r = n % 10;
-        if (arr[r])
-        {
-            break;
-        }
-        arr[r] = true;
-        n = n / 10;
+    int i,r,c=0;
+    for(i=n1;i<=n2;i++)
+    {  
+          bool arr[10] = {false};
+          int num=i;
+      while(num>0)
+      { 
+          r=num%10;
+          if(arr[r])
+          {break;}
+          arr[r]=true;
+          num=num/10;
+      }
+           if(num==0)
+           c=c+1;
+         
+
     }
+   if(c>0)
+    cout<<c;
+else
+cout<<"no"; 
 
-    if (n == 0)
-        cout << "YEs";
-    else
-        cout << "no";
+
     return 0;
 }
